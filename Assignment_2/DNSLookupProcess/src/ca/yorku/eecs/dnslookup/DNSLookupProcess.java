6b29a20e88a0298435cc0381f89d352de5d6c812
Nadim <samnad14@gmail.com>
2022-02-27 23:02:02 -0500

    added starting code for buildQuery()

--- Assignment_2/DNSLookupProcess/src/ca/yorku/eecs/dnslookup/DNSLookupProcess.java
+++ Assignment_2/DNSLookupProcess/src/ca/yorku/eecs/dnslookup/DNSLookupProcess.java
@@ -1,6 +1,8 @@
 package ca.yorku.eecs.dnslookup;
 
 import java.io.Closeable;
+import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -8,6 +10,7 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.security.SecureRandom;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 
 @SuppressWarnings("unused")
@@ -25,16 +28,20 @@ public class DNSLookupProcess implements Closeable {
     private InetAddress nameServer;
 
     /**
-     * Creates a new lookup service. Also initializes the datagram socket object with a default timeout.
+     * Creates a new lookup service. Also initializes the datagram socket object
+     * with a default timeout.
      *
-     * @param nameServer The nameserver to be used initially. If set to null, "root" or "random", will choose a random
+     * @param nameServer The nameserver to be used initially. If set to null, "root"
+     *                   or "random", will choose a random
      *                   pre-determined root nameserver.
-     * @param listener   A DNSProcessListener listener object with methods to be called at key events in the query
+     * @param listener   A DNSProcessListener listener object with methods to be
+     *                   called at key events in the query
      *                   processing.
      * @throws SocketException      If a DatagramSocket cannot be created.
      * @throws UnknownHostException If the nameserver is not a valid server.
      */
-    public DNSLookupProcess(String nameServer, DNSProcessListener listener) throws SocketException, UnknownHostException {
+    public DNSLookupProcess(String nameServer, DNSProcessListener listener)
+            throws SocketException, UnknownHostException {
         this.listener = listener;
         socket = new DatagramSocket();
         socket.setSoTimeout(SO_TIMEOUT);
@@ -53,7 +60,8 @@ public class DNSLookupProcess implements Closeable {
     /**
      * Updates the nameserver to be used in all future queries.
      *
-     * @param nameServer The nameserver to be used initially. If set to null, "root" or "random", will choose a random
+     * @param nameServer The nameserver to be used initially. If set to null, "root"
+     *                   or "random", will choose a random
      *                   pre-determined root nameserver.
      * @throws UnknownHostException If the nameserver is not a valid server.
      */
@@ -75,12 +83,16 @@ public class DNSLookupProcess implements Closeable {
     }
 
     /**
-     * Finds all the result for a specific node. If there are valid (not expired) results in the cache, uses these
-     * results, otherwise queries the nameserver for new records. If there are CNAME records associated to the question,
-     * they are included in the results as CNAME records (i.e., not queried further).
+     * Finds all the result for a specific node. If there are valid (not expired)
+     * results in the cache, uses these
+     * results, otherwise queries the nameserver for new records. If there are CNAME
+     * records associated to the question,
+     * they are included in the results as CNAME records (i.e., not queried
+     * further).
      *
      * @param question Host and record type to be used for search.
-     * @return A (possibly empty) set of resource records corresponding to the specific query requested.
+     * @return A (possibly empty) set of resource records corresponding to the
+     *         specific query requested.
      */
     public Collection<ResourceRecord> getDirectResults(DNSQuestion question) {
 
@@ -93,73 +105,105 @@ public class DNSLookupProcess implements Closeable {
     }
 
     /**
-     * Finds all the result for a specific node. If there are valid (not expired) results in the cache, uses these
-     * results, otherwise queries the nameserver for new records. If there are CNAME records associated to the question,
-     * they are retrieved recursively for new records of the same type, and the returning set will contain both the
+     * Finds all the result for a specific node. If there are valid (not expired)
+     * results in the cache, uses these
+     * results, otherwise queries the nameserver for new records. If there are CNAME
+     * records associated to the question,
+     * they are retrieved recursively for new records of the same type, and the
+     * returning set will contain both the
      * CNAME record and the resulting addresses.
      *
      * @param question             Host and record type to be used for search.
      * @param maxIndirectionLevels Number of CNAME indirection levels to support.
-     * @return A set of resource records corresponding to the specific query requested.
-     * @throws CnameIndirectionLimitException If the number CNAME redirection levels exceeds the value set in
+     * @return A set of resource records corresponding to the specific query
+     *         requested.
+     * @throws CnameIndirectionLimitException If the number CNAME redirection levels
+     *                                        exceeds the value set in
      *                                        maxIndirectionLevels.
      */
-    public Collection<ResourceRecord> getRecursiveResults(DNSQuestion question, int maxIndirectionLevels) throws CnameIndirectionLimitException {
+    public Collection<ResourceRecord> getRecursiveResults(DNSQuestion question, int maxIndirectionLevels)
+            throws CnameIndirectionLimitException {
 
-        if (maxIndirectionLevels < 0) throw new CnameIndirectionLimitException();
+        if (maxIndirectionLevels < 0)
+            throw new CnameIndirectionLimitException();
 
         Collection<ResourceRecord> directResults = getDirectResults(question);
-        if (directResults.isEmpty() || question.getRecordType() == RecordType.CNAME) return directResults;
+        if (directResults.isEmpty() || question.getRecordType() == RecordType.CNAME)
+            return directResults;
 
         List<ResourceRecord> newResults = new ArrayList<>();
         for (ResourceRecord record : directResults) {
             newResults.add(record);
             if (record.getRecordType() == RecordType.CNAME) {
-                newResults.addAll(getRecursiveResults(new DNSQuestion(record.getTextResult(), question.getRecordType(), question.getRecordClass()), maxIndirectionLevels - 1));
+                newResults.addAll(getRecursiveResults(
+                        new DNSQuestion(record.getTextResult(), question.getRecordType(), question.getRecordClass()),
+                        maxIndirectionLevels - 1));
             }
         }
         return newResults;
     }
 
     /**
-     * Retrieves DNS results from a specified DNS server using the iterative mode. After an individual query is sent and
-     * its response is received (or times out), checks if an answer for the specified host exists. Resulting values
-     * (including answers, nameservers and additional information provided by the nameserver) are added to the cache.
+     * Retrieves DNS results from a specified DNS server using the iterative mode.
+     * After an individual query is sent and
+     * its response is received (or times out), checks if an answer for the
+     * specified host exists. Resulting values
+     * (including answers, nameservers and additional information provided by the
+     * nameserver) are added to the cache.
      * <p>
-     * If after the first query an answer exists to the original question (either with the same record type or an
-     * equivalent CNAME record), the function returns with no further actions. If there is no answer after the first
-     * query but the response returns at least one nameserver, a follow-up query for the same question must be done to
+     * If after the first query an answer exists to the original question (either
+     * with the same record type or an
+     * equivalent CNAME record), the function returns with no further actions. If
+     * there is no answer after the first
+     * query but the response returns at least one nameserver, a follow-up query for
+     * the same question must be done to
      * another nameserver.
      * <p>
-     * Note that nameservers returned by the response contain text records linking to the host names of these servers.
-     * If at least one nameserver provided by the response to the first query has a known IP address (either from this
-     * query or from a previous query), it must be used first, otherwise additional queries are required to obtain the
-     * IP address of the nameserver before it is queried. Only one nameserver must be contacted for the follow-up
+     * Note that nameservers returned by the response contain text records linking
+     * to the host names of these servers.
+     * If at least one nameserver provided by the response to the first query has a
+     * known IP address (either from this
+     * query or from a previous query), it must be used first, otherwise additional
+     * queries are required to obtain the
+     * IP address of the nameserver before it is queried. Only one nameserver must
+     * be contacted for the follow-up
      * query.
      *
-     * @param question      Host name and record type/class to be used for the query.
+     * @param question      Host name and record type/class to be used for the
+     *                      query.
      * @param serverAddress Address of the server to be used for the first query.
      */
     protected void iterativeQuery(DNSQuestion question, InetAddress serverAddress) {
 
         /* TO BE COMPLETED BY THE STUDENT */
+
     }
 
     /**
-     * Handles the process of sending an individual DNS query to a single question. Builds and sends the query (request)
-     * message, then receives and parses the response. Received responses that do not match the requested transaction ID
-     * are ignored. If no response is received after SO_TIMEOUT milliseconds, the request is sent again, with the same
-     * transaction ID. The query should be sent at most MAX_QUERY_ATTEMPTS times, after which the function should return
-     * without changing any values. If a response is received, all of its records are added to the cache.
+     * Handles the process of sending an individual DNS query to a single question.
+     * Builds and sends the query (request)
+     * message, then receives and parses the response. Received responses that do
+     * not match the requested transaction ID
+     * are ignored. If no response is received after SO_TIMEOUT milliseconds, the
+     * request is sent again, with the same
+     * transaction ID. The query should be sent at most MAX_QUERY_ATTEMPTS times,
+     * after which the function should return
+     * without changing any values. If a response is received, all of its records
+     * are added to the cache.
      * <p>
-     * The method listener.beforeSendingQuery() must be called every time a new query message is about to be sent.
+     * The method listener.beforeSendingQuery() must be called every time a new
+     * query message is about to be sent.
      *
-     * @param question      Host name and record type/class to be used for the query.
+     * @param question      Host name and record type/class to be used for the
+     *                      query.
      * @param serverAddress Address of the server to be used for the query.
-     * @return If no response is received, returns null. Otherwise, returns a set of resource records for all
-     * nameservers received in the response. Only records found in the nameserver section of the response are included,
-     * and only those whose record type is NS. If a response is received but there are no nameservers, returns an empty
-     * set.
+     * @return If no response is received, returns null. Otherwise, returns a set of
+     *         resource records for all
+     *         nameservers received in the response. Only records found in the
+     *         nameserver section of the response are included,
+     *         and only those whose record type is NS. If a response is received but
+     *         there are no nameservers, returns an empty
+     *         set.
      */
     protected Set<ResourceRecord> individualQueryProcess(DNSQuestion question, InetAddress serverAddress) {
 
@@ -168,10 +212,14 @@ public class DNSLookupProcess implements Closeable {
     }
 
     /**
-     * Fills a ByteBuffer object with the contents of a DNS query. The buffer must be updated from the start (position
-     * 0). A random transaction ID must also be generated and filled in the corresponding part of the query. The query
-     * must be built as an iterative (non-recursive) request for a regular query with a single question. When the
-     * function returns, the buffer's position (`queryBuffer.position()`) must be equivalent to the size of the query
+     * Fills a ByteBuffer object with the contents of a DNS query. The buffer must
+     * be updated from the start (position
+     * 0). A random transaction ID must also be generated and filled in the
+     * corresponding part of the query. The query
+     * must be built as an iterative (non-recursive) request for a regular query
+     * with a single question. When the
+     * function returns, the buffer's position (`queryBuffer.position()`) must be
+     * equivalent to the size of the query
      * data.
      *
      * @param queryBuffer The ByteBuffer object where the query will be saved.
@@ -181,20 +229,45 @@ public class DNSLookupProcess implements Closeable {
     protected int buildQuery(ByteBuffer queryBuffer, DNSQuestion question) {
 
         /* TO BE COMPLETED BY THE STUDENT */
+        int randomNum = ThreadLocalRandom.current().nextInt(0, 65535 + 1); // generate ID for query header
+        queryBuffer.putShort((short) randomNum); // adding ID to ByetBuffer object
+        queryBuffer.putShort((short) 0); // QR OPCODE AA TC RD RA Z RCODE
+        queryBuffer.putShort((short) 1); // QDCOUNT
+        queryBuffer.putShort((short) 0); // ANCOUNT
+        queryBuffer.putShort((short) 0); // NSCOUNT
+        queryBuffer.putShort((short) 0);// ARCOUNT
+
+        // adding question to queryBuffer
+
+        String[] hostName = question.getHostName().split("\\."); // split hostname into array of strings
+        for (int i = 0; i < hostName.length; i++) {
+            queryBuffer.put((byte) hostName[i].length());  // adding length of each string to buffer
+            queryBuffer.put(hostName[i].getBytes());       // adding current part of hostname to buffer
+        }
+        queryBuffer.put((byte) 0); // adding null byte to end of hostname
+        
+
         return 0;
     }
 
     /**
-     * Parses and processes a response received by a nameserver. Adds all resource records found in the response message
-     * to the cache. Calls methods in the listener object at appropriate points of the processing sequence. Must be able
-     * to properly parse records of the types: A, AAAA, NS, CNAME and MX (the priority field for MX may be ignored). Any
-     * other unsupported record type must create a record object with the data represented as a hex string (see method
+     * Parses and processes a response received by a nameserver. Adds all resource
+     * records found in the response message
+     * to the cache. Calls methods in the listener object at appropriate points of
+     * the processing sequence. Must be able
+     * to properly parse records of the types: A, AAAA, NS, CNAME and MX (the
+     * priority field for MX may be ignored). Any
+     * other unsupported record type must create a record object with the data
+     * represented as a hex string (see method
      * byteArrayToHexString).
      *
-     * @param responseBuffer The ByteBuffer associated to the response received from the server.
-     * @return A set of resource records for all nameservers received in the response. Only records found in the
-     * nameserver section of the response are included, and only those whose record type is NS. If there are no
-     * nameservers, returns an empty set.
+     * @param responseBuffer The ByteBuffer associated to the response received from
+     *                       the server.
+     * @return A set of resource records for all nameservers received in the
+     *         response. Only records found in the
+     *         nameserver section of the response are included, and only those whose
+     *         record type is NS. If there are no
+     *         nameservers, returns an empty set.
      */
     protected Set<ResourceRecord> processResponse(ByteBuffer responseBuffer) {
 
@@ -203,8 +276,10 @@ public class DNSLookupProcess implements Closeable {
     }
 
     /**
-     * Helper function that converts a hex string representation of a byte array. May be used to represent the result of
-     * records that are returned by the nameserver but not supported by the application (e.g., SOA records).
+     * Helper function that converts a hex string representation of a byte array.
+     * May be used to represent the result of
+     * records that are returned by the nameserver but not supported by the
+     * application (e.g., SOA records).
      *
      * @param data a byte array containing the record data.
      * @return A string containing the hex value of every byte in the data.
